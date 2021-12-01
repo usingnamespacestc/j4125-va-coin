9c6c7759794d56a14480b8369ef948460aae7717
usingnamespacestc <usingnamespacestc@gmail.com>
2021-11-30 21:03:43 -0500

    优化了sqlController里main的输出，略微修改了代码格式

--- sqlController.py
+++ sqlController.py
@@ -104,7 +104,7 @@ def update_data(coin_data):
                         "_total_supply=" + total_supply + "," +
                         "_last_updated=" + last_updated +
                         ";"
-        )
+                        )
         quote = coin_data[single_coin_data]['quote']
         # print(quote)
     execute_sql(sql_list)
@@ -120,9 +120,6 @@ if __name__ == '__main__':
     #             "_int = 67, "
     #             "_bigint = 35234143151, "
     #             "_timestamp = 1367107200.0;"]
-    # test_ql = "drop table test"
-    # test_sql = [
-    #     "INSERT INTO crypto(_id,_name,_symbol,_num_market_pairs,_date_added,_max_supply,_circulating_supply,_total_supply,_last_updated) VALUES(1027,'Ethereum','ETH',4671,1438905600.0,None,118554060.3115,118554060.3115,1638308468.0) ON DUPLICATE KEY UPDATE _name='Ethereum',_symbol='ETH',_num_market_pairs=4671,_date_added=1438905600.0,_max_supply=None,_circulating_supply=118554060.3115,_total_supply=118554060.3115,_last_updated=1638308468.0;"]
     test_sql = ["select * from crypto"]
     for one in execute_sql(test_sql):
         print(one)
